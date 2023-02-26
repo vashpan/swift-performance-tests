78dc12ae96eba200e50c3d32f03713d958c7cb50
Konrad Kołakowski <vashpan@gmail.com>
2023-02-26 18:04:18 +0100

    App: Initial app mechanism No benchmarks yet though

---
+++ Sources/SwiftPerformance/Benchmarks/Benchmark.swift
@@ -0,0 +1,15 @@
+//
+//  Benchmark.swift
+//  SwiftPerformance
+//
+//  Created by Konrad Kołakowski on 26/02/2023.
+//  Copyright © 2023 One Minute Games. All rights reserved.
+//
+
+public protocol Benchmark {
+    var description: String { get }
+    var expectedResult: Int { get }
+
+    func runAsBoomer(data: [Person]) -> Int
+    func runAsZoomer(data: [Person]) -> Int
+}
--- Sources/SwiftPerformance/Main.swift
+++ Sources/SwiftPerformance/Main.swift
@@ -9,7 +9,65 @@
 import Foundation
 
 @main public struct App {
+    // MARK: Properties
+    private static let version = "1.0.0"
+
+    private static let seed: UInt64 = 580340432343
+    private static let sizeOfDatabase = 1_000_000
+    private static let testIterations = 10_000
+
+    private static let benchmarks: [any Benchmark] = [
+        // todo: add some benchmarks here
+    ]
+
+    // MARK: Start app
     public static func main() {
-        print("I'll be studying performance!")
+        Self.writeBanner()
+
+        var rng = SeedableRandomGenerator(seed: Self.seed)
+
+        // generate some data
+        write("Generating data... ")
+        let persons = PersonsGenerator.generatePersons(amount: Self.sizeOfDatabase, rng: &rng)
+        writeLine("Done!\n")
+
+        // perform benchmarks
+        writeLine("Performing \(Self.benchmarks.count) benchmarks...\n")
+
+        let stopwatch = Stopwatch()
+        for b in benchmarks {
+            stopwatch.start()
+            write(b.description + " (boomer)")
+            let boomerResult = b.runAsBoomer(data: persons)
+            let boomerTime = stopwatch.stop()
+            writeLine("\t👴: \(Self.timeFormatted(boomerTime))")
+
+            stopwatch.start()
+            write(b.description + " (zoomer)")
+            let zoomerResult = b.runAsZoomer(data: persons)
+            let zoomerTime = stopwatch.stop()
+            writeLine("\t👦: \(Self.timeFormatted(zoomerTime))")
+
+            guard boomerResult == zoomerResult else {
+                error("Unexpected result!")
+                break
+            }
+
+            writeLine()
+        }
+    }
+
+    // MARK: Helpers
+    private static func writeBanner() {
+        writeLine("Swift Performance Benchmarks \(Self.version)\n")
+    }
+
+    private static func timeFormatted(_ timeInterval: TimeInterval) -> String {
+        return String(format: "%0.3f s", timeInterval)
+    }
+
+    private static func error(_ msg: String) {
+        writeLine("Error: \(msg)")
+        exit(1)
     }
 }
